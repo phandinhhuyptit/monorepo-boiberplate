6811b8720fb36af8a93b1b026230ba5f16d9acf4
Huy Phan <huydev@Huys-MacBook-Pro.local>
2023-03-18 12:52:54 +0700

    config monorepo

---
+++ libs/core-ui/.storybook/addons/toggle-nightmode-addon/src/preset/addDecorator.js
@@ -0,0 +1,8 @@
+
+import { useWithDarkmode } from '../withDarkmode'
+
+export const decorators = [useWithDarkmode]
+
+export const globals = {
+  "PARAM_KEY": false,
+}
---
+++ libs/core-ui/src/lib/input/Input.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { Input } from './Input';
+
+const Story: Meta<typeof Input> = {
+  component: Input,
+  title: 'Widgets/Input',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
